e16a31c4ce92cb95f087bbc7a0db9a87ec3aaef6
quocnobita <86842314+quocnobita@users.noreply.github.com>
2021-09-27 15:28:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Cục Cưng à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Nguyễn Thị Thanh Thảo ơiiii!',
+    introDesc: `Ngày 15/9 có 1 thiên thần được sinh ra
+Sẽ chẳng thể tìm được một món quà sinh nhật nào đủ tuyệt vời để xứng đáng với m đâu
+Những lời nói muộn màng nhưng t thật sự nhớ ngày sinh của m, chỉ là ko nhớ để chúc.. Xin lỗi..
+Không biết m mong ước điều gì. Mong mày mãi mỉm cười, vì mày cười đẹp lắm.. `,
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Vui và không giận Bún Bò nữa <33',
+    btnNo: 'Không,vẫn giận :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Bé Thảo nhiều nhiều 😘😘',
-    messDesc: 'Đợi anh vài năm nữa rồi cưới nhé .',
+    btnReply: 'Gửi choThanh Thảo <3',
+    reply: 'Cảm ơn Bún Bò, yêu Bún Bò lắm <33333333',
+    mess: 'Tui biết mà 🥰. Yêu Thanh Thảo nhiều nhiều 😘😘 Mãi xinh đẹp nha',
+    messDesc: 'Học thật tốt và luôn vui nhé .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/4080632451990933/' //https://www.facebook.com/messages/t/100006330816944/link mess của các bạn. VD: https://m.me/nam.nodemy
 }
